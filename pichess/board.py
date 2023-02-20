b99ba2b3d1d341aa10171612fa39b7f3760458b0
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-02-20 23:18:41 +0100

    feature: set board position from fen

--- pichess/board.py
+++ pichess/board.py
@@ -1,5 +1,7 @@
 from pichess.square import Square
+from pichess.pieces import Piece
 from PyQt6.QtWidgets import QWidget
+import pichess_engine as engine
 
 
 class Board(QWidget):
@@ -12,8 +14,15 @@ class Board(QWidget):
             for y in range(8):
                 Square(x, y, self)
 
+        # set starting position
+        starting_fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+        board_map = engine.fen_to_board(starting_fen)
+        for coordinates, piece_letter in board_map.items():
+            Piece.from_letter(piece_letter, coordinates, self)
+
         # set size
         self.resize(800, 800)
+
 
     def get_square_by_coordinates(self, coordinates):
         return self.findChild(Square, coordinates)
--- pichess/pieces.py
+++ pichess/pieces.py
@@ -12,6 +12,8 @@ class Piece(QSvgWidget):
       'Pawn': 'P'
     }
 
+    letter_piece_map = {v: k for k, v in piece_letter_map.items()}
+
     def __init__(self, coordinates, color, parent):
         super().__init__(parent)
         self.color = color
@@ -35,6 +37,12 @@ class Piece(QSvgWidget):
         square = board.get_square_by_coordinates(coordinates)
         x, y = map(int, (square.x(), square.y()))
         self.move(x, y)
+
+    @staticmethod
+    def from_letter(letter, coordinates, parent):
+        color = letter.isupper() # color is white if letter is uppercase
+        piece = Piece.letter_piece_map[letter.upper()]
+        return globals()[piece](coordinates, color, parent)
 
 
 # dynamically create chess pieces
